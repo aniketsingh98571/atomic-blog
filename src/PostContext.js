a5f3c051e8b113f7bab8ffeeba60959285cd5467
Aniket Singh <thor98571@gmail.com>
2023-12-25 21:16:46 +0530

    added context api

---
+++ src/PostContext.js
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+//1. Create a new context 
+const PostContext=createContext()
+function PostProvider({children}){
+    const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+  function createRandomPost() {
+    return {
+      title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+      body: faker.hacker.phrase(),
+    };
+  }
+  return (
+     //2. Provide value to the child component
+    <PostContext.Provider value={{posts:searchedPosts,onClearPosts:handleClearPosts,onAddPost:handleAddPost,searchQuery:searchQuery,setSearchQuery:setSearchQuery}}>
+        {children}
+    </PostContext.Provider>
+  )
+}
+export function usePosts(){
+    const context=useContext(PostContext)
+    return context
+}
+export default PostProvider
